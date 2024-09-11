00b8d63350fb195b83cd77050bf7f8427724efc9
LuisDani <spartandanii117@gmail.com>
2024-09-10 21:55:33 -0600

    foto de usuario en los post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,17 @@
         <div class="mt-8">
             @foreach($posts as $post)
                 <div class="border p-4 mb-4">
+                    <div class="flex gap-2 w-auto">
+                        @if($post->user->profile_image)
+                            <img src="{{ asset('storage/' . $post->user->profile_image) }}" alt="Profile Image" class="w-10 h-10 rounded-full object-cover">
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
+                                </svg>
+                        @endif
+                        <p class="text-sm mt-2">{{ $post->user->name }}</p>
+                    </div>
                     <p>{{ $post->text }}</p>
                     @if($post->image)
                         <img src="{{ asset('storage/' . $post->image) }}" alt="Imagen" class="mt-2 w-auto h-auto">
@@ -32,7 +43,7 @@
                             Your browser does not support the video tag.
                         </video>
                     @endif
-                    <p class="text-sm mt-2">Publicado por {{ $post->user->name }}</p>
+                    
                 </div>
             @endforeach
         </div>
