1a04e22ca2877e25981251760174dd01315b3788
LuisDani <spartandanii117@gmail.com>
2024-09-12 21:08:22 -0600

    Vista de los demas usuarios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,7 +30,7 @@
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path>
                                 </svg>
                         @endif
-                        <p class="text-sm mt-2">{{ $post->user->name }}</p>
+                        <a href="{{ route('users.profile', $post->user->id) }}" class="text-sm mt-2 text-blue-500 hover:underline">{{ $post->user->name }}</a>
                     </div>
                     <p>{{ $post->text }}</p>
                     @if($post->image)
