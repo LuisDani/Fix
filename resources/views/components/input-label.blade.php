ee9f766ac789b3f49b5fbfa85e1e1c60afe610a0
DessireZavala <dessirefer01@gmail.com>
2024-09-13 11:09:23 -0600

    fondo de los formularios

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block font-black text-md text-black']) }}>
     {{ $value ?? $slot }}
 </label>
