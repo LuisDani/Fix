05a46b16e3a1dc9ee1762914bc858282ed25ce12
LuisDani <spartandanii117@gmail.com>
2024-09-12 21:43:06 -0600

    Test

--- resources/views/profile/userProfile.blade.php
+++ resources/views/profile/userProfile.blade.php
@@ -2,14 +2,15 @@
     @section('title', 'Perfil de ' . $user->name)
 
     <div class="container mx-auto mt-4">
-        <div class="profile-info-container">
+        <!-- Información del perfil -->
+        <div class="profile-info-container flex py-6 px-5 rounded-lg" style="background: #dfe2e9;">
             <div class="flex items-center gap-4">
                 @if($user->profile_image)
                     <img src="{{ asset('storage/' . $user->profile_image) }}" alt="Profile Image" class="w-24 h-24 rounded-full object-cover">
                 @else
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 2.37A7 7 0 0 0 8 1z"></path>
                     </svg>
                 @endif
                 <div>
@@ -23,7 +24,8 @@
             </div>
         </div>
 
-        <div class="mt-8">
+        <!-- Publicaciones del usuario -->
+        <div class="mt-8 flex flex-col items-center">
             <h3 class="text-xl font-semibold mb-4">Publicaciones de {{ $user->name }}</h3>
 
             @if($posts->count())
